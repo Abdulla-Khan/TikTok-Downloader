03c7d897ee18393d9ebcc2d3f852f9c8dbe93639
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2023-09-13 19:51:23 +0500

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { tiktokDL } from './src/tiktokDL.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import axios from "axios";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -26,3 +26,29 @@ app.listen(port, () => {
     console.log(`${process.env.RAILWAY_STATIC_URL}`)
     console.log(`server actived - port: ${port}`);
 })
+
+export const tiktokDL = async url => {
+    let domain = 'https://www.tikwm.com/';
+    let res = await axios.post(domain+'api/', {}, {
+        headers: {
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            // 'cookie': 'current_language=en; _ga=GA1.1.115940210.1660795490; _gcl_au=1.1.669324151.1660795490; _ga_5370HT04Z3=GS1.1.1660795489.1.1.1660795513.0.0.0',
+            'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+        },
+        params: {
+            url: url,
+            count: 12,
+            cursor: 0,
+            web: 1,
+            hd: 1
+        }
+    })
+
+    return {
+        nowm: domain+res.data.data.play, 
+        wm: domain+res.data.data.wmplay, 
+        music: domain+res.data.data.music, 
+    }
+}
